cd486dea54215f56affa38f2868497d869ed8973
nikitka <nikitausufi00@gmail.com>
2024-12-07 22:39:19 +0200

    изменил css в компоненте cart и сделал изменение в хуке, котоырй запрежает скролить страницу

--- src/hooks/UseDisableScroll.ts
+++ src/hooks/UseDisableScroll.ts
@@ -4,11 +4,14 @@ const useDisableScroll = (item: boolean) => {
   useEffect(() => {
     if (item) {
       document.body.style.overflow = "hidden";
+      document.body.style.marginRight = "16px";
     } else {
       document.body.style.overflow = "";
+      document.body.style.marginRight = "0px";
     }
     return () => {
       document.body.style.overflow = "";
+      document.body.style.marginRight = "0px";
     };
   }, [item]);
 };
--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 
 import style from "./cart.module.css";
 import arrow_button_buy_product from "../../assets/icons/arrow_button_buy_product.svg";
@@ -17,11 +17,7 @@ interface CartProps {
 }
 
 const Cart = ({ showCart, closeCart }: CartProps) => {
-  const [animateCart, setAnimateCart] = useState(false);
-
   const { data: productsCart = [] } = useGetCartProductsQuery();
-
-  const windowForm = useRef<HTMLDivElement>(null);
 
   const totlaPrice: number = productsCart.reduce(
     (total, item) => total + item.price * item.count,
@@ -31,122 +27,103 @@ const Cart = ({ showCart, closeCart }: CartProps) => {
   const navigate = useNavigate();
 
   const handleNavigateToPage = (page: string) => {
-    setAnimateCart(false);
-    handleAnimationEnd();
+    closeCart();
     navigate(page === routes.main ? routes.main : routes.order);
   };
 
+  const windowForm = useRef<HTMLDivElement>(null);
+
   useDisableScroll(showCart); // Отключение скролла при открытии
 
-  useClickOutside(windowForm, () => setAnimateCart(false)); // Закрытие окна при клике вне его
-
-  useEffect(() => {
-    if (showCart) {
-      setAnimateCart(true);
-    } else {
-      setAnimateCart(false);
-    }
-  }, [showCart]);
-
-  const handleAnimationEnd = () => {
-    if (!animateCart) {
-      closeCart();
-    }
-  };
+  useClickOutside(windowForm, closeCart); // Закрытие окна при клике вне его
 
   return (
     <div>
-      {(showCart || animateCart) && (
-        <div className={`${style.blur} ${animateCart ? style.show : ""}`}>
-          <div
-            className={`${style.cart} ${animateCart ? style.show : ""}`}
-            onTransitionEnd={handleAnimationEnd} // Событие завершения анимации
-            ref={windowForm}
-          >
-            {/* кол-во товаров в корзине */}
-            {productsCart.length >= 1 && (
-              <div className="text-xl flex justify-between items-center px-7 py-5">
-                <div>
-                  В корзине{" "}
-                  <span className="font-bold">
-                    {productsCart.length} товара
-                  </span>
-                </div>
+      <div className={`${style.blur} ${showCart && style.active}`}></div>
+      <div
+        className={`${style.cart} ${showCart && style.active}`}
+        ref={windowForm}
+      >
+        {/* кол-во товаров в корзине */}
+        {productsCart.length >= 1 && (
+          <div className="text-xl flex justify-between items-center px-7 py-5">
+            <div>
+              В корзине{" "}
+              <span className="font-bold">{productsCart.length} товара</span>
+            </div>
 
-                <img
-                  src={close_cart}
-                  alt="close cart"
-                  className="cursor-pointer"
-                  onClick={() => setAnimateCart(false)}
-                />
-              </div>
-            )}
-            {/* кол-во товаров в корзине */}
-
-            {/* карточки товаров в корзине */}
-            {productsCart.length >= 1 && (
-              <div className={style.cart_products}>
-                {productsCart.map((item) => (
-                  <CartCard key={item.id} {...item} />
-                ))}
-              </div>
-            )}
-            {/* карточки товаров в корзине */}
-
-            {/*  блок с итоговой ценной и кнопкой перехода к странице оформления заказа */}
-            {productsCart.length >= 1 && (
-              <div className={style.cart_button_buy}>
-                <div className="flex justify-between items-center">
-                  <div>Итого :</div>
-                  <div className="font-bold">{totlaPrice} ₽</div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <div>Налог 5% :</div>
-                  <div className="font-bold">
-                    {Math.round(totlaPrice * 0.05)} ₽
-                  </div>
-                </div>
-                <button
-                  className="bg-[#FE5F00] text-white rounded-[18px] py-3 flex justify-center items-center relative"
-                  onClick={() => handleNavigateToPage(routes.order)}
-                >
-                  <span> Оформить заказ</span>
-                  <img
-                    src={arrow_button_buy_product}
-                    alt="arrow button"
-                    className="absolute right-20 top-1/2 transform -translate-y-1/2"
-                  />
-                </button>
-              </div>
-            )}
-            {/*  блок с итоговой ценной и кнопкой перехода к странице оформления заказа */}
-
-            {/* контент, при пустой корзины */}
-            {productsCart.length < 1 && (
-              <div className=" flex flex-col justify-center items-center h-full">
-                <img src={empty_cart} alt="empty cart" />
-                <div className="text-xl mt-2 mb-1">Корзина пустая</div>
-                <div className="text-gray-500 text-center w-[285px]">
-                  Добавьте хотя бы одну пиццу, чтобы совершить заказ
-                </div>
-
-                <button
-                  className="bg-[#FE5F00] w-[230px] h-[48px] text-white rounded-[18px] py-3 flex justify-center items-center relative mt-6 "
-                  onClick={() => handleNavigateToPage(routes.main)}
-                >
-                  <span> Вернуться назад</span>
-                  <img
-                    src={arrow_button_buy_product}
-                    alt="arrow button"
-                    className="absolute left-7 top-1/2 transform -translate-y-1/2 rotate-180"
-                  />
-                </button>
-              </div>
-            )}
-            {/* контент, при пустой корзины */}
+            <img
+              src={close_cart}
+              alt="close cart"
+              className="cursor-pointer"
+              onClick={closeCart}
+            />
           </div>
-        </div>
-      )}
+        )}
+        {/* кол-во товаров в корзине */}
+
+        {/* карточки товаров в корзине */}
+        {productsCart.length >= 1 && (
+          <div className={style.cart_products}>
+            {productsCart.map((item) => (
+              <CartCard key={item.id} {...item} />
+            ))}
+          </div>
+        )}
+        {/* карточки товаров в корзине */}
+
+        {/*  блок с итоговой ценной и кнопкой перехода к странице оформления заказа */}
+        {productsCart.length >= 1 && (
+          <div
+            className={`${style.cart_button_buy} ${showCart && style.active}`}
+          >
+            <div className="flex justify-between items-center">
+              <div>Итого :</div>
+              <div className="font-bold">{totlaPrice} ₽</div>
+            </div>
+            <div className="flex justify-between items-center">
+              <div>Налог 5% :</div>
+              <div className="font-bold">{Math.round(totlaPrice * 0.05)} ₽</div>
+            </div>
+            <button
+              className="bg-[#FE5F00] text-white rounded-[18px] py-3 flex justify-center items-center relative"
+              onClick={() => handleNavigateToPage(routes.order)}
+            >
+              <span> Оформить заказ</span>
+              <img
+                src={arrow_button_buy_product}
+                alt="arrow button"
+                className="absolute right-20 top-1/2 transform -translate-y-1/2"
+              />
+            </button>
+          </div>
+        )}
+        {/*  блок с итоговой ценной и кнопкой перехода к странице оформления заказа */}
+
+        {/* контент, при пустой корзины */}
+        {productsCart.length < 1 && (
+          <div className=" flex flex-col justify-center items-center min-h-screen">
+            <img src={empty_cart} alt="empty cart" />
+            <div className="text-xl mt-2 mb-1">Корзина пустая</div>
+            <div className="text-gray-500 text-center w-[285px]">
+              Добавьте хотя бы одну пиццу, чтобы совершить заказ
+            </div>
+
+            <button
+              className="bg-[#FE5F00] w-[230px] h-[48px] text-white rounded-[18px] py-3 flex justify-center items-center relative mt-6 "
+              onClick={() => handleNavigateToPage(routes.main)}
+            >
+              <span> Вернуться назад</span>
+              <img
+                src={arrow_button_buy_product}
+                alt="arrow button"
+                className="absolute left-7 top-1/2 transform -translate-y-1/2 rotate-180"
+              />
+            </button>
+          </div>
+        )}
+        {/* контент, при пустой корзины */}
+      </div>
     </div>
   );
 };
